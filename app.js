cb689b9a244c529928aa5216200c34673133d8da
OluEkele <ekeleolu1@gmail.com>
2023-12-18 10:31:09 +0100

    First commit

--- app.js
+++ app.js
@@ -64,3 +64,22 @@ function sendMail(){
     
 }
 
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("homecontentslide");
+  let dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" native", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " native";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
